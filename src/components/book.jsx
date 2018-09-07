3bab5c30b2224c772fefdaafc46b4c80dbfd4818
Hannah Lee <leexhannah@gmail.com>
2018-09-07 14:03:25 -0700

    Style Book component.

--- src/components/book.jsx
+++ src/components/book.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Book = props => {
   return (
     <div className="book">
-      <span>
-        Author: {props.author}, Title: {props.bookTitle}, Description:{" "}
-        {props.description}, Rank: {props.rank}, Weeks on the List:
-        {props.weeksOnList}
-      </span>
+      {/* <p>ISBN: {props.isbn}</p> */}
+      <p style={{ fontSize: 24, fontWeight: "bold" }}>{props.bookTitle}</p>
+      <p style={{ fontSize: 18 }}>{props.description}</p>
+      <p style={{ fontSize: 18 }}>by {props.author}</p>
+      <p>
+        Rank: {props.rank}, Weeks on the List: {props.weeksOnList}
+      </p>
     </div>
   );
 };
