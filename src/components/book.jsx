1343be618653b714ccbb401197cb85718bb2d036
Hannah Lee <leexhannah@gmail.com>
2018-09-10 23:08:11 -0700

    Refactor Book component from functional to class component, and return New This Week or number of Weeks On The List.

--- src/components/book.jsx
+++ src/components/book.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Book = props => {
-  return (
-    <li className="book">
-      {/* <p>ISBN: {props.isbn}</p> */}
-      {/* <p className="book-rank">{props.rank}</p> */}
-      <p className="book-week">Weeks on the List: {props.weeksOnList}</p>
-      <p className="book-title">{props.bookTitle.toUpperCase()}</p>
-      <p className="book-author">by {props.author} | {props.publisher}</p>
-      <p className="book-description">{props.description}</p>
-    </li>
+class Book extends Component {
+  getWeeksOnList() {
+    return this.props.weeksOnList === 1
+      ? "NEW THIS WEEK"
+      : this.props.weeksOnList + " WEEKS ON THE LIST";
+  }
 
-  );
-};
+  render(props) {
+    return (
+      <li className="book">
+        {/* <p>ISBN: {props.isbn}</p> */}
+        {/* <p className="book-rank">{props.rank}</p> */}
+        <p className="book-week">{this.getWeeksOnList()}</p>
+        <p className="book-title">{this.props.bookTitle.toUpperCase()}</p>
+        <p className="book-author">
+          by {this.props.author} | {this.props.publisher}
+        </p>
+        <p className="book-description">{this.props.description}</p>
+      </li>
+    );
+  }
+}
 
 export default Book;
